7764066f1266ce33b6bdcb1163e85a324f86c312
Nathan Chan <nathanchan@Nathans-MacBook-Air.local>
2019-06-03 10:10:02 -0700

    added comments

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
-// Credit to http://danorlovsky.tech/Articles/Javascript-Hangman-Tutorial
+// Credit to http://danorlovsky.tech/Articles/Javascript-Hangman-Tutorial for some code
 
+// Bugs to be fixed:
+// 1. If you solve it, it doesn't reset
+// 2. Spaces present an issue
+// 3. Image isn't updating properly
 
 // Variable declarations
-var words = ["seattle", "california", "washington", "berkeley"];
+var words = ["seattle", "california", "washington", "berkeley", "i am coding at ga"];
 var correctWord =  words[Math.floor(Math.random() * words.length)];
 var blanks = correctWord.split('').fill("_");
 var guessedLetters = "";
@@ -19,6 +23,8 @@ var btnEl2 = document.getElementById("reset");
 
 
 // Create all event listeners
+
+// Guess button
 btnEl.addEventListener("click", function (e) {
     var letterInput = inputLetter.value;
     inputLetter.value = "";
@@ -27,6 +33,7 @@ btnEl.addEventListener("click", function (e) {
     pEl2.textContent = guessedLetters;
 })
 
+// Reset button
 btnEl2.addEventListener("click", function (e2) {
     guessedLetters = "";
     triesLeft = 7;
@@ -43,6 +50,8 @@ btnEl2.addEventListener("click", function (e2) {
 
 
 // Any additional functions
+
+// Make my guess
 var guess = function (letter, correctWord) {
     if ( !guessedLetters.includes(letter) ) {
         guessedLetters.concat(letter);
@@ -52,18 +61,21 @@ var guess = function (letter, correctWord) {
     }
 }
 
+// Update the image
 var hangmanImage = function() {
     document.getElementById("hangmanImage").src = "img/" + triesLeft +".jpg";
 }
 
+
+// Check if the letter is in the word
 var checkLetters = function (letter, correctWord) {
     var arr = correctWord.split('');
     var indices = [];
-    indices = getAllIndexes(arr, letter);
-    if (indices.length <= 0) {
+    indices = getAllIndexes(arr, letter); // Stores all indexes of the correct letter in indices
+    if (indices.length <= 0) { // If the indices array is 0 or less which means the correct letter has not been identified
         triesLeft--;
         pEl3.textContent = "You have " + triesLeft + " tries left!";
-        hangmanImage();
+        hangmanImage(); //Update the image
         if (triesLeft <= 0) {
             pEl3.textContent = "Game Over! Sorry try again!";
             return;
@@ -72,7 +84,7 @@ var checkLetters = function (letter, correctWord) {
         //     return;
         // }
     } else {
-        indices.forEach( function (index) {
+        indices.forEach( function (index) { // Fill in the blanks
             blanks[index] = letter;
         });
         pEl.textContent = blanks.join(' '); 
@@ -109,6 +121,7 @@ var checkLetters = function (letter, correctWord) {
 
 
 // Credit for the following function to https://stackoverflow.com/questions/20798477/how-to-find-index-of-all-occurrences-of-element-in-array
+// Get indexes of location of the correct letters
 function getAllIndexes(arr, val) {
     var indexes = [];
     for(i = 0; i < arr.length; i++)
@@ -117,5 +130,6 @@ function getAllIndexes(arr, val) {
     return indexes;
 }
 
+// Run the guess function based on the word list and the input letter
 guess(letterInput, correctWord);
 
